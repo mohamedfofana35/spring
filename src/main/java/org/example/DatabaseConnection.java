84bb7eeefad9d3abcc5fc339762a9187511d1f1b
mohamedfofana35 <djiba2k24@gmail.com>
2024-11-05 18:30:44 +0100

    Update DatabaseConnection.java

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
     // Informations de connexion
-    private static final String URL = "jdbc:postgresql://localhost:5432/demo";
+    private static final String URL = "jdbc:postgresql://localhost:5432/nomdb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "1994";
+    private static final String PASSWORD = "xxxx";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
